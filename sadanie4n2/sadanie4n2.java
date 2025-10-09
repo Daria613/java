1c0db7af6b13505e249cefcda19ab79ac90433a2
Daria613 <batrakovva9@gmail.com>
2025-10-09 21:04:38 +0300

    Add files via upload

---
+++ sadanie4n2/sadanie4n2.java
@@ -0,0 +1,10 @@
+public class sadanie4n2 {
+    public static void main(String[] args) {
+
+        double a=1;
+        double c=1;
+        double b =1;
+        double m = 0.5 * Math.sqrt(3 * Math.pow(b, 2) + 2 * Math.pow(c, 2) + Math.pow(a, 2));
+          System.out.println(m);
+    }
+}
